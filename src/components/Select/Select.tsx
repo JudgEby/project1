affbb76d3279aae39992c8332d124c19d286dc76
Yan Dobrovolskiy <bloodwoodwood@gmail.com>
2021-07-03 18:34:53 +0300

    added yarn.lock to gitignore

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { KeyboardEvent, useState } from 'react'
 import style from './Select.module.css'
 
 export type SelectType = {
@@ -14,13 +14,16 @@ export type ItemType = {
 
 export const Select = ({ value, items, onChange }: SelectType) => {
   const [collapsed, setCollapsed] = useState<boolean>(true)
+  const [hoveredElement, setHoveredElement] = useState(value)
+
+  const hoveredItem = items.find((item) => item.valueArr === hoveredElement)
 
   const getOptions = () => {
     if (!collapsed && items) {
       return (
         <>
           <div
-            className={style.item}
+            className={`${style.item} ${style.none}`}
             onClick={() => {
               onChange(null)
               setCollapsed(!collapsed)
@@ -31,10 +34,15 @@ export const Select = ({ value, items, onChange }: SelectType) => {
           {items.map((item, idx) => {
             return (
               <div
-                className={style.item}
+                className={`${style.item} ${
+                  hoveredItem === item ? style.highlighted : ''
+                }`}
                 key={idx}
+                onMouseEnter={() => setHoveredElement(item.valueArr)}
+                onMouseLeave={() => setHoveredElement(null)}
                 onClick={() => {
                   onChange(item.valueArr)
+                  setHoveredElement(item.valueArr)
                   setCollapsed(!collapsed)
                 }}
               >
@@ -53,8 +61,43 @@ export const Select = ({ value, items, onChange }: SelectType) => {
     return titleObj ? titleObj.title : 'chose option'
   }
 
+  const onKeyDown = (e: KeyboardEvent<HTMLDivElement>) => {
+    const step = e.key === 'ArrowUp' ? -1 : e.key === 'ArrowDown' ? 1 : null
+    const selectedNow = items.findIndex((item) => item.valueArr === value)
+    if (step) {
+      const changeByStep = () => {
+        onChange(items[selectedNow + step].valueArr)
+        setHoveredElement(items[selectedNow + step].valueArr)
+      }
+      if (step === 1) {
+        if (selectedNow === -1 || selectedNow === items.length - 1) {
+          onChange(items[0].valueArr)
+          setHoveredElement(items[0].valueArr)
+        } else {
+          changeByStep()
+        }
+      } else {
+        if (selectedNow === -1 || selectedNow === 0) {
+          onChange(items[items.length - 1].valueArr)
+          setHoveredElement(items[items.length - 1].valueArr)
+        } else {
+          changeByStep()
+        }
+      }
+    }
+
+    if (e.key === 'Enter') {
+      if (!collapsed) {
+        onChange(items[selectedNow].valueArr)
+        setHoveredElement(items[selectedNow].valueArr)
+      }
+
+      setCollapsed(!collapsed)
+    }
+  }
+
   return (
-    <div className={style.select}>
+    <div className={style.select} onKeyDown={onKeyDown} tabIndex={0}>
       <div
         className={style.title}
         onBlur={() => !collapsed && setCollapsed(!collapsed)}
